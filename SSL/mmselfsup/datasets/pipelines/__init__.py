d2124d797738f1afcde84f5feb4df793bfe82df8
SupCodeTech <gs63891@student.upm.edu.my>
2022-10-08 18:26:40 +0800

    Y2

--- SSL/mmselfsup/datasets/pipelines/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .transforms import (BEiTMaskGenerator, GaussianBlur, Lighting,
-                         RandomAppliedTrans, RandomAug, SimMIMMaskGenerator,
-                         Solarization, ToTensor)
-
-__all__ = [
-    'GaussianBlur', 'Lighting', 'RandomAppliedTrans', 'Solarization',
-    'RandomAug', 'SimMIMMaskGenerator', 'ToTensor', 'BEiTMaskGenerator'
-]
