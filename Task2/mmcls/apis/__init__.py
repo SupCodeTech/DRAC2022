744920cdbe73640039c748f96001729253b575a5
SupCodeTech <gs63891@student.upm.edu.my>
2022-12-17 01:46:16 +0800

    New

--- Task2/mmcls/apis/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .inference import inference_model
-from .model import ModelHub, get_model, init_model, list_models
-
-__all__ = [
-    'init_model', 'inference_model', 'list_models', 'get_model', 'ModelHub'
-]
