f06c306babe92afeb7529b6d96fbbd751db8010f
SupCodeTech <111235455+SupCodeTech@users.noreply.github.com>
2023-03-02 19:57:26 +0800

    Update MCS_DRNet_Task_1_Mask_B_1536x1536.py

--- Task1/tools/MCS_DRNet_Task_1_Mask_B_1536x1536.py
+++ Task1/tools/MCS_DRNet_Task_1_Mask_B_1536x1536.py
@@ -31,44 +31,27 @@ from mmseg.utils import collect_env, get_root_logger, setup_multi_processes
  
 def main():
   parser = ArgumentParser()
-  parser.add_argument('checkpointM', help='Image file')
-  parser.add_argument('checkpointC', help='Image file')
-  parser.add_argument('checkpointS', help='file')
-  parser.add_argument('output_data_dir', help='MAE Config file')
-  # parser.add_argument('checkpoint', help='Checkpoint file')
+  
+  parser.add_argument('load_from_C_config', help='path to store the config of the pretrained model ConvNeXt')
+  parser.add_argument('load_from_checkpoint_C', help='path to store the checkpoints of the pretrained model ConvNeXt')
+  parser.add_argument('output_data_dir', help='output path of test segmentation results')
   parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
   args = parser.parse_args()
-
-  config_file_MAE_Mask_A = '/content/drive/MyDrive/mmsegmentation-0.24.1/configs/mae/upernet_mae-base_fp16_512x512_160k_drac_ms_SSL.py'
-  # config_file_convnext_XL_Mask_A = './configs/MCS_DRNet/C_Task_1_Mask_A_640x640.py'
-  # config_file_segFormer_Mask_A = './configs/MCS_DRNet/S_Task_1_Mask_A_1024x1024.py'
   
-  M = init_segmentor(config_file_MAE_Mask_A, args.checkpointM,  device='cuda:0')
-  # C = init_segmentor(config_file_convnext_XL_Mask_A, checkpoint_file_ConvNeXt_XL_Mask_A,  device='cuda:0')
-  # S = init_segmentor(config_file_segFormer_Mask_A, checkpoint_file_SegFormer_Mask_A,  device='cuda:0')
+  C = init_segmentor(args.load_from_checkpoint_C_config, args.load_from_checkpoint_C,  device='cuda:0')
 
   image_size = 1536
 
-  test_data_dir = '/content/drive/MyDrive/DRAC2022/Task1/DRAC2022_dataset/Test_Data/1536/'
+  test_data_dir = './DRAC2022_dataset/Test_Data/1536/'
 
   test_data_dir_1 = test_data_dir + 'Original_Images/'
-  # test_data_dir_2 = test_data_dir + 'Horizontal_flip/'
-  # test_data_dir_3 = test_data_dir + 'Vertical_flip/'
-  # test_data_dir_4 = test_data_dir + 'R90/'
-  # test_data_dir_5 = test_data_dir + 'R180/'
-  # test_data_dir_6 = test_data_dir + 'R270/'
-
+  
   Nonperfusion_Areas_Mask_path = args.output_data_dir + '/Nonperfusion_Areas'
   Nonperfusion_Areas_Mask_path_folder = os.path.exists(Nonperfusion_Areas_Mask_path)
   if not Nonperfusion_Areas_Mask_path_folder:
-    print("创建了文件夹")
     os.makedirs(Nonperfusion_Areas_Mask_path)
 
-  # Neovascularization_Mask_path = output_data_dir + '/Neovascularization_Mask'
-  # Neovascularization_Mask_path_folder = os.path.exists(Neovascularization_Mask_path)
-  # if not Neovascularization_Mask_path_folder:
-  #   os.makedirs(Neovascularization_Mask_path)
 
   for file in mmcv.scandir(test_data_dir_1, suffix='.png'):
     
@@ -76,20 +59,17 @@ def main():
 
     Nonperfusion_Areas_Mask = np.zeros(2359296).reshape((image_size, image_size)) 
 
-    result_1 = inference_segmentor(M, image_file_1)
-    # result_2 = inference_segmentor(C, image_file_1)
-    # result_3 = inference_segmentor(S, image_file_1)
+    result_ = inference_segmentor(C, image_file_1)
 
     for i in range(image_size):
       for j in range(image_size):
-        if result_1[0][i][j] > 0:
-          Nonperfusion_Areas_Mask[i][j] = 25
+        if result_[0][i][j] > 0:
+          Nonperfusion_Areas_Mask[i][j] = 255
 
     # Intraretinal_microvascular_abnormals_Mask
     # 1536 position recovery
     Nonperfusion_Areas_Mask = Image.fromarray(Nonperfusion_Areas_Mask).convert('P')
     Nonperfusion_Areas_Mask = Nonperfusion_Areas_Mask.resize((1024, 1024), Image.ANTIALIAS)
-    print("^^^")
     Nonperfusion_Areas_Mask = np.asarray(Nonperfusion_Areas_Mask)
 
     for i in range(1024):
